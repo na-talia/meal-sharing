baefa197f822aad116a73f535cae3eae50545438
na-talia <ntsh_89@ukr.net>
2023-02-01 22:02:16 +0100

    meal-sharing endpoints

--- src/backend/app.js
+++ src/backend/app.js
@@ -7,6 +7,7 @@ const mealsRouter = require("./api/meals");
 const buildPath = path.join(__dirname, "../../dist");
 const port = process.env.PORT || 3000;
 const cors = require("cors");
+const knex = require("./database");
 
 // For week4 no need to look into this!
 // Serve the built client html
@@ -21,10 +22,90 @@ app.use(cors());
 
 router.use("/meals", mealsRouter);
 
+app.get("/my-route", (req, res) => {
+  res.send("Hi friend");
+});
+
+//Respond with all meals in the future (relative to the when datetime)
+
+app.get("/future-meals", async (req, res) => {
+  try {
+    const [result] = await knex.raw(
+      "SELECT title, description FROM meal WHERE meal.when > NOW()"
+    );
+    res.send(result); // an array of objects
+  } catch (error) {
+    console.error(error.message);
+    res.send("Something went wrong");
+  }
+});
+
+//Respond with all meals in the past (relative to the when datetime)
+
+app.get("/past-meals", async (req, res) => {
+  try {
+    const [result] = await knex.raw(
+      "SELECT title, description FROM meal WHERE meal.when < NOW()"
+    );
+    res.send(result); // an array of objects
+  } catch (error) {
+    console.error(error.message);
+    res.send("Something went wrong");
+  }
+});
+
+//Respond with all meals sorted by ID
+
+app.get("/all-meals", async (req, res) => {
+  try {
+    const [result] = await knex.raw(
+      "SELECT title, description FROM meal ORDER BY id"
+    );
+    res.send(result); // an array of objects
+  } catch (error) {
+    console.error(error.message);
+    res.send("Something went wrong");
+  }
+});
+
+//Respond with the first meal (meaning with the minimum id)
+
+app.get("/first-meal", async (req, res) => {
+  try {
+    const [result] = await knex.raw("SELECT * FROM meal ORDER BY id LIMIT 1");
+    if (result.length === 0) {
+      res.status(404).send("There are no meals");
+    } else {
+      res.json(result[0]); // an object
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.send("Something went wrong");
+  }
+});
+
+//Respond with the last meal (meaning with the maximum id)
+
+app.get("/last-meal", async (req, res) => {
+  try {
+    const [result] = await knex.raw(
+      "SELECT * FROM meal ORDER BY id DESC LIMIT 1"
+    );
+    if (result.length === 0) {
+      res.status(404).send("There are no meals");
+    } else {
+      res.json(result[0]); // an object
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.send("Something went wrong");
+  }
+});
+
 if (process.env.API_PATH) {
   app.use(process.env.API_PATH, router);
 } else {
-  throw "API_PATH is not set. Remember to set it in your .env file"
+  throw "API_PATH is not set. Remember to set it in your .env file";
 }
 
 // for the frontend. Will first be covered in the react class
