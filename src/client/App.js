0492720d9dcc19b39f5b9114d2aaaf17363ed81e
na-talia <ntsh_89@ukr.net>
2023-03-11 01:08:41 +0100

    update App.js file

--- src/client/App.js
+++ src/client/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import TestComponent from "./components/TestComponent/TestComponent";
+import MealList from "./components/MealList";
 
 function App() {
   return (
     <Router>
       <Route exact path="/">
-        <p>test</p>
+        <MealList />
       </Route>
       <Route exact path="/lol">
         <p>lol</p>
