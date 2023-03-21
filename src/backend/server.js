26d1c27c5a5cb9d786bf13428391c638c1b1a5cc
na-talia <ntsh_89@ukr.net>
2023-03-21 18:21:17 +0100

    update config files

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,6 +2,6 @@ const app = require("./app");
 
 const port = parseInt(process.env.PORT, 10) || process.env.API_PORT;
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Backend api available at ${process.env.API_PATH}`);
 });
