29a8c9754c68b3de3482595d09801f748914df72
na-talia <ntsh_89@ukr.net>
2023-03-28 15:45:31 +0200

    add Navbar, Title, Footer and About

--- src/client/components/NavBar.js
+++ src/client/components/NavBar.js
@@ -1,10 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import NavbarCSS from "./UI/main.module.css";
 
 const NavBar = () => {
   return (
-    <div>
-      <Link to="/">Meals</Link>
+    <div className={NavbarCSS.navbar}>
+      <Link to="/">
+        <li>Home</li>
+      </Link>
+      <Link to="/meals">
+        <li>All Meals</li>
+      </Link>
+      <Link to="/about">
+        <li>About us</li>
+      </Link>
     </div>
   );
 };
